f55c4d2e1a86e586667f777518580e48dc2f2117
Carsten Gips <carsten.gips@fh-bielefeld.de>
2016-09-02 13:05:44 +0200

    add support for inline lecture notes

--- texnotes.py
+++ texnotes.py
@@ -19,22 +19,45 @@ notesEnd   = re.compile('\\\\notesend')
 
 content = False
 
+
 def texnotes(key, value, format, meta):
     global content
+
     if key == 'Para' and isinstance(value, list) and len(value) == 1:
+        # handle '\notesbegin' and '\notesbegin' appearing in its own paragraph
         item = value[0]
         if isinstance(item, dict) and 't' in item:
             if item["t"] == 'RawInline':
-                fmt, s = item["c"]
-                if fmt == "tex":
-                    if notesStart.search(s):
-                        content = True
-                        return []
-                    elif notesEnd.search(s):
-                        content = False
-                        return []
+                return filternotes(item["c"])
+    elif key == 'RawInline':
+        # handle '\notesbegin' and '\notesbegin' appearing in the same paragraph
+        return filternotes(value)
+
+    return returncontent()
+
+
+def filternotes(value):
+    global content
+
+    fmt, s = value
+    if fmt == "tex":
+        if notesStart.search(s):
+            content = True
+            return []
+        elif notesEnd.search(s):
+            content = False
+            return []
+
+    return returncontent()
+
+
+def returncontent():
+    global content
+
     if content:
         return []
+
+
 
 if __name__ == "__main__":
     toJSONFilter(texnotes)
