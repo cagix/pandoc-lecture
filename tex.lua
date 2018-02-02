81ed2313d005c28034c8a2a2db7a1e33915592b9
Carsten Gips <carsten.gips@fh-bielefeld.de>
2018-02-02 15:10:27 +0100

    add handler for inline and block notes

--- tex.lua
+++ tex.lua
@@ -7,5 +7,25 @@ function center(el)
 end
 
 
-return {{Div = center}}
+-- there is a Note class in Pandoc Lua ... (inline class)
+function notes(el)
+    return {}
+end
+
+-- remove inline notes: `[...]{.notes}` ... (Span class)
+function inlineNotes(el)
+    if el.classes[1] == "notes" then
+        return {}
+    end
+end
+
+-- remove block notes: `::: notes ... :::` ... (Div class)
+function blockNotes(el)
+    if el.classes[1] == "notes" then
+        return {}
+    end
+end
+
+
+return {{Div = center}, {Note = notes, Span = inlineNotes, Div = blockNotes}}
 
