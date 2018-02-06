9ad1ba9d03b54c9775f8645b68575fbbc68df031
Carsten Gips <carsten.gips@fh-bielefeld.de>
2018-02-06 10:37:16 +0100

    refactoring: extract and use helper function

--- filters/tex.lua
+++ filters/tex.lua
@@ -3,6 +3,14 @@
 -- License: MIT
 
 
+-- helper function to insert two string as RawInline into a table
+local function insertLatexEnvInline(tab, strBegin, strEnd)
+    table.insert(tab, 1,      pandoc.RawInline("latex",strBegin))
+    table.insert(tab, #tab+1, pandoc.RawInline("latex",strEnd))
+    return tab
+end
+
+
 -- handling of `::: center ... :::` ... (Div class)
 function center(el)
     if el.classes[1] == "center" then
@@ -29,10 +37,7 @@ end
 -- handling of  `[...]{.alert}` ... (Span class)
 function alert(el)
     if el.classes[1] == "alert" then
-        local erg = el.content
-        table.insert(erg, 1,      pandoc.RawInline("latex","\\alert{"))
-        table.insert(erg, #erg+1, pandoc.RawInline("latex","}"))
-        return erg
+        return insertLatexEnvInline(el.content, "\\alert{", "}")
     end
 end
 
@@ -40,10 +45,7 @@ end
 -- handling of  `[...]{.bsp}` ... (Span class)
 function bsp(el)
     if el.classes[1] == "bsp" then
-        local erg = el.content
-        table.insert(erg, 1,      pandoc.RawInline("latex","\\bsp{"))
-        table.insert(erg, #erg+1, pandoc.RawInline("latex","}"))
-        return erg
+        return insertLatexEnvInline(el.content, "\\bsp{", "}")
     end
 end
 
@@ -51,10 +53,7 @@ end
 -- handling of  `[...]{.cbox}` ... (Span class)
 function cbox(el)
     if el.classes[1] == "cbox" then
-        local erg = el.content
-        table.insert(erg, 1,      pandoc.RawInline("latex","\\cboxbegin"))
-        table.insert(erg, #erg+1, pandoc.RawInline("latex","\\cboxend"))
-        return erg
+        return insertLatexEnvInline(el.content, "\\cboxbegin", "\\cboxend")
     end
 end
 
