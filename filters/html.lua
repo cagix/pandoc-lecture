2670203a2b650199391df40461cfc3562bbebb91
Carsten Gips <carsten.gips@fh-bielefeld.de>
2018-02-14 18:45:08 +0100

    add function to handle `\blueArrow` in block context

--- filters/html.lua
+++ filters/html.lua
@@ -3,15 +3,31 @@
 -- License: MIT
 
 
+-- helper function to create span for blue arrows
+local function blueArrow()
+    return {pandoc.Span({pandoc.Str("=>")}, pandoc.Attr("", {"blueArrow"})), pandoc.Space()}
+end
+
+
 -- handling of `\blueArrow` ... (RawInline, tex)
-function blueArrow(el)
-    if el.format == "tex" then
+function blueArrowInline(el)
+    if el.format == "tex" or el.format == "latex" then
         if string.match(el.text, "\\blueArrow") then
-            return {pandoc.Span({pandoc.Str("=>")}, pandoc.Attr("", {"blueArrow"})), pandoc.Space()}
+            return blueArrow()
         end
     end
 end
 
 
-return {{RawInline = blueArrow}}
+-- handling of `\blueArrow` ... (RawBlock, tex)
+function blueArrowBlock(el)
+    if el.format == "tex" or el.format == "latex"  then
+        if string.match(el.text, "\\blueArrow") then
+            return pandoc.Plain(blueArrow())
+        end
+    end
+end
+
+
+return {{RawInline = blueArrowInline, RawBlock = blueArrowBlock}}
 
