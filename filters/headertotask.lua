1f40214aca022c361d0b765a4748ee3315acfd1d
Carsten Gips <cagix@fh-bielefeld.de>
2020-08-19 17:29:55 +0200

    refactoring: improve readability of headertotask.lua

--- filters/headertotask.lua
+++ filters/headertotask.lua
@@ -2,14 +2,15 @@
 -- add some TeX code to headers with `{punkte=42}`
 function Header(el)
     local p = tonumber(el.attributes["punkte"]) or 0
+    local c = el.content
 
     if p > 0 then
-        table.insert(el.content, pandoc.Space())
-        table.insert(el.content, pandoc.RawInline("latex", "\\hfill"))
-        table.insert(el.content, pandoc.Space())
-        table.insert(el.content, pandoc.Str("(" .. p))
-        table.insert(el.content, pandoc.Space())
-        table.insert(el.content, pandoc.Str("Punkt" .. (p > 1 and "e" or "") .. ")"))
+        c:insert(pandoc.Space())
+        c:insert(pandoc.RawInline("latex", "\\hfill"))
+        c:insert(pandoc.Space())
+        c:insert(pandoc.Str("(" .. p))
+        c:insert(pandoc.Space())
+        c:insert(pandoc.Str("Punkt" .. (p > 1 and "e" or "") .. ")"))
     end
 
     return el
