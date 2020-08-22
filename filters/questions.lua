1570a02e84c313ef10a1fd0d0d0035002678923e
Carsten Gips <cagix@fh-bielefeld.de>
2020-08-22 16:49:27 +0200

    refactoring: improve readability of questions.lua

--- filters/questions.lua
+++ filters/questions.lua
@@ -6,13 +6,10 @@ local questions = {}
 -- generate table of all question headers
 function collectQuestionHeaders(el)
     local p = tonumber(el.attributes["punkte"]) or 0
-    local c = el.content
 
     -- collect only questions with points ...
     if p > 0 then
-        c:insert(pandoc.Space())
-        c:insert(pandoc.Str("(" .. p .. "P)"))
-        questions[#questions + 1] = pandoc.MetaInlines(c)
+        questions[#questions + 1] = pandoc.MetaInlines(el.content .. {pandoc.Space(), pandoc.Str("(" .. p .. "P)")})
     end
 end
 
