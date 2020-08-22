db8b23dae8867a3a37117cdbe0b64967aa46bec3
Carsten Gips <cagix@fh-bielefeld.de>
2020-08-22 16:13:47 +0200

    refactoring: use Div.attributes shortcut instead of Div.attr.attributes, same with Image

--- filters/stripImageAttrs.lua
+++ filters/stripImageAttrs.lua
@@ -1,21 +1,27 @@
 
 -- strip scaling information from images attrs
 --
--- the images are often explicitly scaled based on the absolute dimensions in
+-- the images are often explicitly scaled based using absolute dimensions in
 -- the markdown sources, which is quite usefull when emitting beamer slides (pdf).
 -- however, absolute dimensions can cause problems when generating html, as this
 -- will not match the rest of the style (e.g. page width).
 --
 -- we are therefore temporarily using manually scaled png images for more predictable
--- results in html
+-- results in html: save png at width=600px (max. 800px), downscaling is done via CSS
+--
 -- strip also "width" from Div (div.column) to avoid column overlap on small screens
-function stripImageAttrs(el)
-    el.attr.attributes["scale"] = nil
-    el.attr.attributes["width"] = nil
-    el.attr.attributes["height"] = nil
+
+function Image(el)
+    el.attributes["scale"] = nil
+    el.attributes["width"] = nil
+    el.attributes["height"] = nil
 
     return el
 end
 
 
-return { { Div = stripImageAttrs, Image = stripImageAttrs } }
+function Div(el)
+    el.attributes["width"] = nil
+
+    return el
+end
