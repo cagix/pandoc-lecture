bb8e40fa9c688bb665ae794fbde5f648b287d0a4
Carsten Gips <cagix@fh-bielefeld.de>
2021-09-02 19:49:52 +0200

    TeX: do not remove markdown escapings like `el`{=markdown}
    this is used to prevent Pandoc from over-escaping when preprocessing
    Markdown content before feeding it to Hugo ... now, when using Pandoc
    to create Beamer slides from the same content, we do not want to
    discard this escaped content but to use it also in the slides ...
    
    this can cause problems, as shortcodes and Pandoc might be incompatible

--- filters/tex.lua
+++ filters/tex.lua
@@ -5,7 +5,6 @@ local latexCmds = pandoc.List:new {'alert', 'bsp', 'hinweis', 'origin', 'thema'}
 -- LaTeX environments to be handled (matching definitions needed!)
 local latexEnvs = pandoc.List:new {'cbox', 'center'}
 
-
 -- handle selected Spans: embed content into a RawInline with matching LaTeX command
 function Span(el)
     local cmd = el.classes[1]
@@ -15,7 +14,6 @@ function Span(el)
         return {pandoc.RawInline("latex", "\\" .. cmd .. "{")} .. el.content .. {pandoc.RawInline("latex", "}")}
     end
 end
-
 
 -- handle selected Divs: embed content into a RawBlock with matching LaTeX environment
 function Div(el)
@@ -32,5 +30,14 @@ end
 function Image(el)
     if el.caption and #el.caption == 0 then
         return { pandoc.RawInline('latex', '\\begin{center}'), el, pandoc.RawInline('latex', '\\end{center}') }
+    end
+end
+
+
+-- do not remove "`el`{=markdown}", convert it to raw "LaTeX" instead
+-- see https://github.com/KI-Vorlesung/kitest/issues/80
+function RawInline(el)
+    if el.format:match 'markdown' then
+        return pandoc.RawInline('latex', el.text)
     end
 end
