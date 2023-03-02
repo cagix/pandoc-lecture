2511d4609aed3b2cadbf9c5c1cf24d8a465994a8
Carsten Gips <cagix@fh-bielefeld.de>
2023-03-02 07:20:23 +0100

    fix typo: name of downloaded file

--- docker/install-pandoc-lecture.sh
+++ docker/install-pandoc-lecture.sh
@@ -8,4 +8,4 @@ cd ${XDG_DATA_HOME}/pandoc/  &&  rm -rf .git demo/ docker/
 
 
 ## Hugo Relearn Theme: https://github.com/McShelby/hugo-theme-relearn/releases/latest/
-wget https://github.com/McShelby/hugo-theme-relearn/archive/refs/tags/${RELEARN}.tar.gz && tar -zxf hugo-theme-relearn-*.tar.gz --strip-components 1 -C ${XDG_DATA_HOME}/pandoc/hugo/hugo-theme-relearn/ && rm hugo-theme-relearn-*.tar.gz
+wget https://github.com/McShelby/hugo-theme-relearn/archive/refs/tags/${RELEARN}.tar.gz && tar -zxf ${RELEARN}.tar.gz --strip-components 1 -C ${XDG_DATA_HOME}/pandoc/hugo/hugo-theme-relearn/ && rm ${RELEARN}.tar.gz
