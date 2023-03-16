a5719808efa4e5ffaae8e79ca5152f1a4da6e6bb
Carsten Gips <cagix@fh-bielefeld.de>
2023-03-16 17:31:31 +0100

    Update URL to digibib.net

--- filters/fhbib.lua
+++ filters/fhbib.lua
@@ -5,7 +5,8 @@
 -- reference to ISBN 12345 would be linked to https://worldcat.org/isbn/12345
 -- my students would rather like to know availability in university library,
 -- thus change generated URL to https://www.digibib.net/openurl/Bi10?isbn=12345
+-- 03/2023: the URL apparently changed to https://fhb-bielefeld.digibib.net/openurl?isbn=12345 (?)
 function Link(el)
-    el.target = el.target:gsub("^https://worldcat.org/isbn/(%d[0-9%-]+%d)$", "https://www.digibib.net/openurl/Bi10?isbn=%1")
+    el.target = el.target:gsub("^https://worldcat.org/isbn/(%d[0-9%-]+%d)$", "https://fhb-bielefeld.digibib.net/openurl?isbn=%1")
     return el
 end
