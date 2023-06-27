f77f55605940836ddeddf3c5a3b41508c203f759
Carsten Gips <cagix@fh-bielefeld.de>
2023-06-27 11:09:48 +0200

    Relearn: bump version to 5.17.1

--- docker/install-relearn.sh
+++ docker/install-relearn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Versions
-export RELEARN="5.16.0"
+export RELEARN="5.17.1"
 # set XDG_DATA_HOME externally
 
 ## Hugo Relearn Theme: https://github.com/McShelby/hugo-theme-relearn/releases/latest/
