67a124475bc91889a8005eefdfd0d2ab2993fb07
Carsten Gips <cagix@fh-bielefeld.de>
2023-06-28 10:20:08 +0200

    [Action] add optional support for graphviz (#76)

--- docker/install-packages-ubuntu.sh
+++ docker/install-packages-ubuntu.sh
@@ -2,5 +2,5 @@
 
 ## Ubuntu: base packages
 apt-get update
-apt-get install -y ca-certificates bash git make gzip tar wget graphviz
+apt-get install -y ca-certificates bash git make gzip tar wget graphviz ghostscript
 rm -rf /var/lib/apt/lists/*
