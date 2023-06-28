b688ecb1ee64590975d82fe273fce289b524918f
Carsten Gips <cagix@fh-bielefeld.de>
2023-06-28 07:44:55 +0200

    [Docker] remove package "build-essential"
    package "build-essential" is not really needed. it contains "make", but also gcc and several other packages not needed here.

--- docker/install-packages-ubuntu.sh
+++ docker/install-packages-ubuntu.sh
@@ -2,5 +2,5 @@
 
 ## Ubuntu: base packages
 apt-get update
-apt-get install -y ca-certificates build-essential bash git make gzip tar wget
+apt-get install -y ca-certificates bash git make gzip tar wget
 rm -rf /var/lib/apt/lists/*
