fd8833311ca19e478ce7adaa6ef743f323e6c406
Carsten Gips <cagix@fh-bielefeld.de>
2023-07-18 19:39:44 +0200

    merge docker/download-pandoc.sh into docker/install-pandoc.sh

--- docker/download-pandoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## Versions
-export PANDOC="3.1.5"
-# set ARCH="amd64" or ARCH="arm64" externally
-
-## Pandoc: https://github.com/jgm/pandoc/releases/latest/
-## split into two steps: Download + Install to enable reuse in Docker and GitHub actions
-wget https://github.com/jgm/pandoc/releases/download/${PANDOC}/pandoc-${PANDOC}-linux-${ARCH}.tar.gz
--- docker/install-pandoc.sh
+++ docker/install-pandoc.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-## split into two steps: Download + Install to enable reuse in Docker and GitHub actions
+## Versions
+export PANDOC="3.1.5"
+# set ARCH="amd64" or ARCH="arm64" externally
+
+## Pandoc: https://github.com/jgm/pandoc/releases/latest/
+wget https://github.com/jgm/pandoc/releases/download/${PANDOC}/pandoc-${PANDOC}-linux-${ARCH}.tar.gz
+
 tar -zxf pandoc-*.tar.gz --strip-components 1 -C /usr/local/
 rm pandoc-*.tar.gz
