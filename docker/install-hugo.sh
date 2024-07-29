b7dd4340250f9e07548062b19cab11b93facb714
Carsten Gips <cagix@hsbi.de>
2024-07-29 16:21:01 +0200

    hugo: bump version to v0.130.0

--- docker/install-hugo.sh
+++ docker/install-hugo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Versions
-export HUGO="0.129.0"
+export HUGO="0.130.0"
 # set ARCH="amd64" or ARCH="arm64" externally
 export ARCH="${ARCH:-amd64}"  # if not set, use 'amd64' as default
 
