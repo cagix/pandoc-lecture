3117bbe519659671ca57706d3a8802637b4c03b5
Carsten Gips <cagix@hsbi.de>
2025-03-17 09:57:34 +0100

    pandoc: bump version to v3.6.4

--- docker/install-pandoc.sh
+++ docker/install-pandoc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Versions
-export PANDOC="3.6.3"
+export PANDOC="3.6.4"
 # set ARCH="amd64" or ARCH="arm64" externally
 export ARCH="${ARCH:-amd64}"  # if not set, use 'amd64' as default
 
